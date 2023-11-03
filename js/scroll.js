bfd6fdb2a2783fd1cc2a574010aa4e3d69681f54
fabio7621 <d6200121a@gmail.com>
2023-11-03 10:11:32 +0800

    1103

--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,34 @@
 const logopic = document.querySelector(".logopic");
+const drop1 = document.getElementById("dr-1");
+const drop2 = document.getElementById("dr-2");
+const drop3 = document.getElementById("dr-3");
+const drop4 = document.getElementById("dr-4");
+const mainHead = document.getElementById("mainHeader");
+
 let isScrolling = false;
 
 window.addEventListener("scroll", () => {
   if (!isScrolling) {
-    // 如果没有滚动计时器，则设置一个
     isScrolling = true;
 
     requestAnimationFrame(() => {
-      // 根据滚动位置设置宽度
       const scrollPosition = window.scrollY;
       if (scrollPosition > 0) {
         logopic.style.width = "10vw";
+        drop1.style.top = "44px";
+        drop2.style.top = "44px";
+        drop3.style.top = "44px";
+        drop4.style.top = "44px";
+        mainHead.style.opacity = "0.9";
       } else {
         logopic.style.width = "17.3056vw";
+        drop1.style.top = "3vw";
+        drop2.style.top = "3vw";
+        drop3.style.top = "3vw";
+        drop4.style.top = "3vw";
+        mainHead.style.opacity = "1";
       }
 
-      // 重置滚动计时器
       isScrolling = false;
     });
   }
